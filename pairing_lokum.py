150b1766e685a89ce252e98ccca1b7fd32f0b9b7
Onur İnanç <e191322@metu.edu.tr>
2023-04-27 13:54:01 +0300

    having issues with ate_pairing function due to Ker(pi-1) is not equal

--- pairing_lokum.py
+++ pairing_lokum.py
@@ -5,6 +5,7 @@ from sage.rings.rational_field import QQ
 from sage.misc.functional import cyclotomic_polynomial
 from sage.rings.finite_rings.finite_field_constructor import FiniteField, GF
 from sage.schemes.elliptic_curves.constructor import EllipticCurve
+from sage.schemes.elliptic_curves.ell_point import *
 
 # this is much much faster with this statement:
 # proof.arithmetic(False)
@@ -13,228 +14,7 @@ from sage.structure.proof.all import arithmetic
 from pairing import *
 from pairing_cp6_782 import *
 from test_pairing import *
-
-
-def ate_pairing_cp6_782(Q, P, a, tr):
-    T = tr-1
-    m,S1 = miller_function_ate(Q, P, a, T)
-    f = final_exp_cp6_782(m)
-    return f
-
-
-def final_exp_hard_cp6_782(m):
-    # returns m^(-W0 + q*W1) = (m^(-1))^W0 * (m^q)^W1
-    W1_2naf=[0,1,0,1,0,-1,0,0,-1,0,-1,0,-1,0,1,0,0,0,1,0,1,0,0,-1,0,0,-1,0,0,0,0,-1,0,-1,0,-1,0,0,-1,0,0,1,0,-1,0,0,-1,0,0,0,0,-1,0,-1,0,1,0,-1,0,-1,0,-1,0,-1,0,0,0,0,-1,0,-1,0,-1,0,0,0,1,0,1,0,1,0,1,0,1,0,-1,0,0,0,-1,0,0,0,1,0,-1,0,0,0,0,-1,0,1,0,-1,0,1,0,1,0,-1,0,1,0,-1,0,0,0,0,1,0,1,0,0,0,0,-1,0,0,0,1,0,1,0,0,1,0,0,0,1,0,-1,0,1,0,-1,0,0,-1,0,-1,0,0,1,0,0,-1,0,0,1,0,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1,0,-1,0,0,0,-1,0,1,0,0,0,0,1,0,-1,0,0,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,-1,0,-1,0,0,0,1,0,1,0,1,0,0,0,0,0,-1,0,0,0,-1,0,1,0,-1,0,0,0,-1,0,1,0,-1,0,0,0,1,0,0,-1,0,0,0,0,-1,0,-1,0,-1,0,-1,0,1,0,1,0,0,1,0,0,0,0,-1,0,1,0,0,1,0,-1,0,-1,0,1,0,0,1,0,0,0,0,-1,0,0,-1,0,1,0,-1,0,-1,0,0,-1,0,1,0,-1,0,1,0,0,-1,0,0,0,0,0,0,-1,0,1,0,0,0,0,0,0,1,0,0,-1,0,1,0,0,0,-1,0,0,1,0,0,0,1,0,-1,0,-1,0,-1,0,0,0,0,1,0,1,0,0,0,-1,0,1,0,0,1,0,1,0,0,0,0,-1,0,0,0,0,0,1,0,0,1,0,-1,0,-1,0,0,-1,0,0,0,0,0,-1,0,1,0,0,0,1]
-    W0_2naf=[1,0,0,0,1,0,-1,0,0,-1,0,-1,0,-1,0,1,0,0,0,1,0,1,0,0,-1,0,0,-1,0,0,0,0,-1,0,-1,0,-1,0,0,-1,0,0,1,0,-1,0,0,0,-1,0,0,0,-1,0,1,0,0,-1,0,-1,0,0,-1,0,-1,0,-1,0,0,0,0,0,1,0,0,0,1,0,0,1,0,1,0,0,1,0,-1,0,0,1,0,1,0,0,1,0,-1,0,0,0,-1,0,0,0,1,0,-1,0,0,-1,0,-1,0,0,-1,0,-1,0,0,-1,0,0,0,1,0,-1,0,-1,0,0,0,1,0,1,0,0,0,0,1,0,-1,0,0,1,0,0,1,0,1,0,0,1,0,0,0,0,0,-1,0,1,0,0,-1,0,0,0,1,0,-1,0,0,0,1,0,-1,0,0,0,0,0,0,0,0,-1,0,1,0,0,1,0,-1,0,0,1,0,1,0,0,1,0,0,0,0,1,0,1,0,0,-1,0,-1,0,0,1,0,0,0,0,-1,0,0,0,-1,0,-1,0,1,0,1,0,0,-1,0,1,0,-1,0,-1,0,0,0,0,1,0,1,0,0,-1,0,0,-1,0,0,-1,0,-1,0,0,-1,0,1,0,-1,0,-1,0,1,0,-1,0,0,1,0,0,0,0,1,0,-1,0,1,0,-1,0,-1,0,0,0,1,0,-1,0,0,0,0,1,0,0,0,1,0,0,0,-1,0,1,0,0,-1,0,0,-1,0,1,0,0,0,1,0,0,0,1,0,1,0,0,1,0,-1,0,0,0,-1,0,0,-1,0,1,0,0,1,0,1,0,1,0,0,0,-1,0,0,0,0,0,1,0,-1,0,0,1,0,0,0,1,0,1,0,0,0,1,0,-1,0,1,0,-1,0,0,0,1,0,0,0,1,0,-1,0,0,1,0,1,0,0,1,0,0,-1,0,0,0,0,-1,0,0,1,0,-1,0,-1,0,1,0,1,0,-1,0,0,0,-1,0,-1,0,0,-1,0,-1,0,1,0,0,0,1,0,-1,0,1,0,0,-1,0,1,0,-1,0,1,0,0,-1,0,0,1,0,1,0,0,-1,0,1,0,-1,0,1,0,0,0,0,0,0,1,0,-1,0,1,0,1,0,0,0,1,0,0,-1,0,1,0,-1,0,-1,0,0,-1,0,0,0,0,0,1,0,1,0,-1,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,1,0,-1,0,1,0,-1,0,1,0,1,0,1,0,-1,0,0,0,1,0,0,-1,0,1,0,0,-1,0,0,0,0,1,0,0,1,0,1,0,-1,0,0,-1,0,0,0,0,0,-1,0,1,0,-1,0,0,-1,0,0,0,-1,0,-1,0,0,1,0,-1,0,0,0,0,1,0,1,0,0,1,0,0,-1,0,-1,0,1,0,0,0,0,0,1,0,-1,0,-1,0,0,0,1,0,0,1,0,-1,0,0,0,-1,0,0,-1,0,-1,0,1,0,1,0,0,-1,0,0,0,0,-1,0,1,0,-1,0,0,0,0,0,0,0,0,0,1,0,-1,0,-1,0,1,0,-1,0,1,0,-1,0,0,1,0,0,0,0,1,0,0,0,0,-1,0,-1,0,-1,0,1,0,0,1,0,-1,0,-1,0,-1,0,-1,0,-1,0,0,1,0,0,1,0,0,1,0,-1,0,-1,0,0,0,1,0,1,0,0,0,1,0,1]
-    f0 = m
-    f0_inv = f0.frobenius(3) # f0^(q^3) = 1/f0
-    f1 = m.frobenius()
-    f1_inv = f1.frobenius(3) # f1^(q^3) = 1/f1
-    f,S,M = multi_exp_2naf(f0_inv,f0,f1,f1_inv,W0_2naf,W1_2naf,verbose=False)
-    return f
-
-def final_exp_cp6_782(m):
-    f = final_exp_easy_k6(m)
-    f = final_exp_hard_cp6_782(f)
-    return f
-
-def final_exp_easy_k6(m):
-    """cost: f3 + i6 + 2*m6 + f
-    """
-    mp3 = m.frobenius(3) # assuming the extension is in one layer
-    im = 1/m
-    f = mp3*im # m^(q^3-1)
-    f = f * f.frobenius() # m^((q^3-1)*(q+1))
-    return f
-
-def test_ate_pairing_cp6_782(E, E6_abs, r, c, E2, c2, tr, D_twist):
-    q = E.base_field().characteristic()    
-    exponent = ((q**6-1) // r)
-    Q2 = c2*E2.random_element()
-    Q = map_psi_M_D_twist(Q2, E6_abs, D_twist)
-    #assert r*Q == E6_abs(0)
-    P = c*E.random_element() # now P has order r
-    #assert r*P == E(0)
-    f = ate_pairing_cp6_782(Q, P, E.a4(), tr)
     
-    f1_ate, S1  = miller_function_ate(Q,P,E.a4(), tr-1)
-    f_ate = f1_ate**exponent
-    f_ate_ = final_exp_cp6_782(f1_ate)
-    ok = f_ate == f_ate_ == f
-    # check it is bilinear
-    #ok = True
-    aa = 1
-    while ok and aa < 6:
-        bb = 1
-        while ok and bb < 6:
-            f_ate2 = ate_pairing_cp6_782(aa*Q, bb*P, E.a4(), tr)
-            ok = ok and (f_ate2 == f**(aa*bb))
-            bb += 1
-        aa += 1
-    print("check that ate pairing is bilinear: {}".format(ok))
-    return ok    
-
-def test_miller_function_ate_cp6_782(E, E6_abs, r, c, E2, c2, tr, D_twist=False):
-    # ate pairing: impossible to check, impossible to compute ate with Magma or Sage
-    q = E.base_field().characteristic()    
-    exponent = ((q**6-1) // r)
-    Q2 = c2*E2.random_element()
-    Q = map_psi_M_D_twist(Q2, E6_abs, D_twist)
-    #assert r*Q == E6_abs(0)
-    P = c*E.random_element() # now P has order r
-    #assert r*P == E(0)
-    f1_ate, S1  = miller_function_ate(Q, P, E.a4(), tr-1)
-    f_ate = f1_ate**exponent
-    f_ate_ = final_exp_cp6_782(f1_ate)
-    ok = f_ate == f_ate_
-    # check it is bilinear
-    #ok = True
-    aa = 1
-    while ok and aa < 6:
-        bb = 1
-        while ok and bb < 6:
-            f_ate2, S1  =  miller_function_ate(aa*Q, bb*P, E.a4(), tr-1)
-            f_ate2 = final_exp_cp6_782(f_ate2) # f_ate2^((q^6-1) // r)
-            ok = ok and (f_ate2 == f_ate**(aa*bb))
-            bb += 1
-        aa += 1
-    print("check that miller_function is bilinear: {}".format(ok))
-    return ok
-
-def test_curve(u, a, b, q, r, tr, c, y, D, xi):
-
-    print("###################################################################")
-    print("test curve E: y^2 = x^3 + a*x + b")
-    print("a = {} b = {:#x}".format(a, b))
-
-    assert q == (tr**2 + D*y**2)//4
-    print("q = {:#x} # {} bits".format(q, q.nbits()))
-    Fq = GF(q)
-    E = EllipticCurve([Fq(a),Fq(b)])
-    res = check_curve_order(E,r*c)
-    print("check_curve_order(E,r*c): {}".format(res))
-    print("ate pairing, Miller loop scalar is tr-1 =\nT = {}\nT = {:#x}".format(tr-1, tr-1))
-    
-    # absolute extension
-    #Fqz.<z> = Fq[]
-    #Fq6_abs.<w> = Fq.extension(z**6 - xi)
-    #Fq3.<j> = Fq.extension(z**3 - xi)
-    #Fq3Z.<Z> = Fq3[]
-    #Fq6.<s> = Fq3.extension(Z**2 - j)
-    Fqz = Fq['z']; (z,) = Fqz._first_ngens(1);
-    Fq6_abs = Fq.extension(z**6 - xi, names=('w',)); (w,) = Fq6_abs._first_ngens(1);
-    Fq3 = Fq.extension(z**3 - xi, names=('j',)); (j,) = Fq3._first_ngens(1);
-    Fq3Z = Fq3['Z']; (Z,) = Fq3Z._first_ngens(1);
-    Fq6 = Fq3.extension(Z**2 - j, names=('s',)); (s,) = Fq6._first_ngens(1)
-
-    #E6 = EllipticCurve([Fq6(a),Fq6(b)])
-    E6_abs = EllipticCurve([Fq6_abs(a),Fq6_abs(b)])
-    
-    # G2
-    # M-twist
-    E_M = EllipticCurve([(Fq3(a))*j**2, (Fq3(b))*j**3])
-    print("M-twist\na=a*w^2={}\nb=b*w^3={}".format(E_M.a4(),E_M.a6()))
-    E_D = EllipticCurve([(Fq3(a))/j**2, (Fq3(b))/j**3])
-    print("D-twist\na=a/w^2={}\nb=b/w^3={}".format(E_D.a4(),E_D.a6()))
-    # Sage does not know how to compute efficiently the curve order over that extension
-    #order_EM = E_M.order()
-    #order_ED = E_D.order()
-    #assert (order_EM % r) == 0
-    #assert (order_ED % r) == 0
-    #assert order_EM == order_ED
-
-    #tr1 = tr
-    #tr2 = tr^2 - 2*q
-    #tr3 = tr*tr2 - q*tr1
-    tr3 = tr**3 - 3*q*tr # order E/Fq3 = (q^3 + 1 - tr3) = (q+1-tr) * (q^2-q+1 + t^2 + q*t + t)
-    order_twist = q**3 + 1 + tr3
-    assert order_twist % r == 0
-    c2 = order_twist // r
-    
-    print("check_curve_order(E_M, order_twist): {}".format(check_curve_order(E_M, order_twist)))
-    print("check_curve_order(E_D, order_twist): {}".format(check_curve_order(E_D, order_twist)))
-    
-    exponent = (q**2 - q + 1)//r
-    e1 = exponent // q
-    e0 = exponent % q
-    
-    e1_2naf = bits_2naf(e1)
-    e0_2naf = bits_2naf(e0)    
-
-# The above functions are used in the cp6_782 pairing impl. I need to transform it into lokum
-# Use miller_function_ate for ate_pairing
-
-
-# To use this function we need a point in Q, P, a, tr
-def ate_pairing_lokum(Q, P, a, tr):
-    T = tr-1
-    m, S1 = miller_function_ate(Q, P, a, T)
-    f = final_exp_lokum(m)
-    return f
-
-def final_exp_lokum(m):
-    f = final_exp_easy_k8(m)
-    f = final_exp_hard_lokum(f)
-    return f
-
-def final_exp_easy_k8(m):
-    # The below parameters needs to be changed
-    mp3 = m.frobenius(3)
-
-
-def test_ate_pairing_lokum(E, E6_abs, r, c, E2, c2, tr, D_twist):
-    q = E.base_field().characteristic()    
-    exponent = ((q**6-1) // r) ## CHANGE q**8-1
-    Q2 = c2*E2.random_element()
-    Q = map_psi_M_D_twist(Q2, E6_abs, D_twist)
-    #assert r*Q == E6_abs(0)
-    P = c*E.random_element() # now P has order r
-    #assert r*P == E(0)
-    f = ate_pairing_cp6_782(Q, P, E.a4(), tr)
-    
-    f1_ate, S1  = miller_function_ate(Q,P,E.a4(), tr-1)
-    f_ate = f1_ate**exponent
-    f_ate_ = final_exp_cp6_782(f1_ate)
-    ok = f_ate == f_ate_ == f
-    # check it is bilinear
-    #ok = True
-    aa = 1
-    while ok and aa < 6:
-        bb = 1
-        while ok and bb < 6:
-            f_ate2 = ate_pairing_cp6_782(aa*Q, bb*P, E.a4(), tr)
-            ok = ok and (f_ate2 == f**(aa*bb))
-            bb += 1
-        aa += 1
-    print("check that ate pairing is bilinear: {}".format(ok))
-    return ok    
-
-def test_miller_function_ate_cp6_782(E, E6_abs, r, c, E2, c2, tr, D_twist=False):
-    # ate pairing: impossible to check, impossible to compute ate with Magma or Sage
-    q = E.base_field().characteristic()    
-    exponent = ((q**6-1) // r)
-    Q2 = c2*E2.random_element()
-    Q = map_psi_M_D_twist(Q2, E6_abs, D_twist)
-    #assert r*Q == E6_abs(0)
-    P = c*E.random_element() # now P has order r
-    #assert r*P == E(0)
-    f1_ate, S1  = miller_function_ate(Q, P, E.a4(), tr-1)
-    f_ate = f1_ate**exponent
-    f_ate_ = final_exp_cp6_782(f1_ate)
-    ok = f_ate == f_ate_
-    # check it is bilinear
-    #ok = True
-    aa = 1
-    while ok and aa < 6:
-        bb = 1
-        while ok and bb < 6:
-            f_ate2, S1  =  miller_function_ate(aa*Q, bb*P, E.a4(), tr-1)
-            f_ate2 = final_exp_cp6_782(f_ate2) # f_ate2^((q^6-1) // r)
-            ok = ok and (f_ate2 == f_ate**(aa*bb))
-            bb += 1
-        aa += 1
-    print("check that miller_function is bilinear: {}".format(ok))
-    return ok
 def check_curve_order(E, order):
     # randomized probabilistic check, usefull for large parameters
     ok = True
@@ -245,32 +25,6 @@ def check_curve_order(E, order):
         i += 1
     return ok
 
-# def test_miller_function_ate_cp6_782(E, E6_abs, r, c, E2, c2, tr, D_twist=False):
-def test_miller_function_ate_lokum(E, E8_abs, r, c, E2, c2, tr, D_twist=False):
-    q = E.base_field().characteristic()    
-    exponent = ((q**8-1) // r)
-    Q2 = c2*E2.random_element()
-    Q = map_psi_M_D_twist(Q2, E6_abs, D_twist)
-    #assert r*Q == E6_abs(0)
-    P = c*E.random_element() # now P has order r
-    #assert r*P == E(0)
-    f1_ate, S1  = miller_function_ate(Q, P, E.a4(), tr-1)
-    f_ate = f1_ate**exponent
-    f_ate_ = final_exp_cp6_782(f1_ate)
-    ok = f_ate == f_ate_
-    # check it is bilinear
-    #ok = True
-    aa = 1
-    while ok and aa < 6:
-        bb = 1
-        while ok and bb < 6:
-            f_ate2, S1  =  miller_function_ate(aa*Q, bb*P, E.a4(), tr-1)
-            f_ate2 = final_exp_cp6_782(f_ate2) # f_ate2^((q^6-1) // r)
-            ok = ok and (f_ate2 == f_ate**(aa*bb))
-            bb += 1
-        aa += 1
-    print("check that miller_function is bilinear: {}".format(ok))
-    return ok
 # Originally -> def test_lokum(u, a, b, q, r, tr, c, y, D, xi):
 def test_lokum(a, b, q, r, tr, c, D):
     print("###################################################################")
@@ -281,8 +35,12 @@ def test_lokum(a, b, q, r, tr, c, D):
     print("q = {:#x} # {} bits".format(q, q.nbits()))
     Fq = GF(q)
     E = EllipticCurve([Fq(a),Fq(b)])
-
+    P = E.random_point()
+    P3 = P + P + P
+    print("P is a random_point on the elliptic curve!")
+    print(P)
     print("The order of the curve is:", c)
+    order_P_Q = r * c
     res = check_curve_order(E,r*c)
     print("check_curve_order(E,r*c): {}".format(res))
     print("ate pairing, Miller loop scalar is tr-1 =\nT = {}\nT = {:#x}".format(tr-1, tr-1))
@@ -310,9 +68,32 @@ def test_lokum(a, b, q, r, tr, c, D):
     Fq4Z = Fq4['Z']; (Z,) = Fq4Z._first_ngens(1);
     Fq8 = Fq4.extension(Z**2 - j, names=('s',)); (s,) = Fq8._first_ngens(1)
 
-    E8_abs = EllipticCurve([Fq8_abs(a),Fq8_abs(b)])
-    print(E8_abs)
+    EK = E.base_extend(Fq8_abs)
+    print("A random point the extension field, Q is: ")
+    Q = EK.random_point()
+    T = EK.random_point()
+    
+    #print("########  ORDER EK #################3")
+    #print(EK.order())
+    print(Q)
+    print(T)
+    Z = Q + T
+    print("Z is an addition of Q to the T")
+    print(Z)
 
+    print("########## ABOUT Q ##############")
+    print(Q)
+    print("Q[0] is:    ")
+    print(Q[0])
+    print("Q[1] is:    ")
+    print(Q[1])
+    print("#### piP is")
+    piP = EK(Q[0]**q, Q[1]**q)
+    print(piP)
+    print(piP - Q)
+    orddd = 11178471414836345336059048855611390994257347541678490001885302105452889531220681887063760522435920242549498857070544319215681595199825520457978318663949088451956393179940626847131008230965896616721895736790011463601842300814881217357911666596763666142060364248293661581607975062108643224429446283393991255035656825117099140533934652875143386959036703603226566679927774482780478792984642946385754975582563741941170313319050311130987979716708470069385452052743295817756387189493033394289373456999033551211492693962602437835427803879042700605665271512616644853266461370443414567029631937208881495158644492620428589143183636655393718868986856362999803778403269108096669598360627088720895292022466546145805613907119407341002305902542929009712010952117502667466294030730061615881003093058836542115915779296546454823737486659498696382465941269092111594805775965304567737873324377973343180166011760630835377760362141121685804988129983376016160652759299142703468026467306556279618698252911728583946615756708459752059357920596901915230169254310895866402942405009235706175972318443692528574476593489954174142202606563337911501692785987709315325523637478127171293449780083027709715228710614005458956061504218741911709094828298418333340827389154912375715843217153929829606277277554688
+    Q.ate_pairing(T, orddd, 8, tr)
+    
     E_D = EllipticCurve([(Fq4(a))/j**2, (Fq4(b))/j**3])
     print("D-twist\na=a/w^2={}\nb=b/w^3={}".format(E_D.a4(),E_D.a6()))
     print("PRINT FQ4")
